d9c068543908f0460c9bcd031594317b54d55d6b
heros20 <heros40@hotmail.fr>
2021-12-21 22:05:02 +0100

    update marque-page

--- asset/js/bookmark.js
+++ asset/js/bookmark.js
@@ -1,9 +1,10 @@
 $(window).on("load", function () {
     const bookmark_id = localStorage.getItem('bookmark_id') || -1;
-    $('.first-P').before((index) => '<div data-index="' + index + '" class="bookmarkss"></div>');
+    $('.first-P').before((index) => '<div data-index="' + index + '" class="bookmarkss" style="text-align:center;color:white;font-weight:bold" ></div>');
 
     if (bookmark_id >= 0) {
       $('*[data-index="' + bookmark_id + '"]').addClass('actived');
+      $('*[data-index="' + bookmark_id + '"]').html('Marque-page');
       $('*[data-index="' + bookmark_id + '"]').attr('id', 'marque_page');
     }
 
@@ -18,23 +19,28 @@ $(window).on("load", function () {
 
 $(function() {
   $('.bookmarkss').click(function(event) {
+    event.stopPropagation()
     const index = $(this).data("index");
+    $(this).html('Marque-page');
     const isActive = $(this).hasClass('actived');
     $('.bookmarkss').removeClass('actived');
+    
       if (isActive) {
       localStorage.removeItem('bookmark_id');
+      $(this).html('');
       } else {
         $('*[data-index="' + index + '"]').addClass('actived');
         localStorage.setItem('bookmark_id', index);
       }
-      event.stopPropagation()
+      
   });
 });
 $(function(){
   var $select = $(".1-100");
-  $select.append($('<li></li>').val(0).html('<a onclick="myFunction(this)" href="#marque_page" style="color:red;">Marque-page</a>'))
+  $select.append($('<li style="color:white" >Choisissez votre page</li>'));
+  $select.append($('<li></li>').val(0).html('<a onclick="myFunction(this)" href="#marque_page" style="color:red;">Marque-page</a>'));
   for (i=1;i<=68;i++){
-      $select.append($('<li></li>').val(i).html('<a onclick="myFunction(this)" href="#page'+ i +'">Page '+ i +'</a>'))
+      $select.append($('<li></li>').val(i).html('<a onclick="myFunction(this)" href="#page'+ i +'">'+ i +'</a>'))
   }
 });
 
