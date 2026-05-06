57d410c3d782de942617613ddb87f937a167b0d6
heros20 <heros40@hotmail.fr>
2026-05-06 23:41:37 +0200

    Transform Book of Mey reader platform

---
+++ asset/js/supabase-config.js
@@ -0,0 +1,4 @@
+window.BOOK_OF_MEY_SUPABASE = {
+  url: "https://dafnshawabjbrxmjletk.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhZm5zaGF3YWJqYnJ4bWpsZXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwOTM2MDcsImV4cCI6MjA5MzY2OTYwN30.BHphThXsFLtg3bwZgYy82yXiOPJTbCIDZZ3I2HUAVjM",
+};
