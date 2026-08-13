8b652b12d5dbc8d92cc73a13f7d921216d7f6fc8
heros20 <heros20@users.noreply.github.com>
2026-08-13 08:26:20 +0000

    deploy: 2345e22aaff98422bc11090bb93e05676b5adaf3

--- asset/js/supabase-config.js
+++ asset/js/supabase-config.js
@@ -1,6 +1,6 @@
 window.BOOK_OF_MEY_SUPABASE = {
   url: "https://dafnshawabjbrxmjletk.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6ImRhZm5zaGF3YWJqYnJ4bWpsZXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwOTM2MDcsImV4cCI6MjA5MzY2OTYwN30.BHphThXsFLtg3bwZgYy82yXiOPJTbCIDZZ3I2HUAVjM",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhZm5zaGF3YWJqYnJ4bWpsZXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwOTM2MDcsImV4cCI6MjA5MzY2OTYwN30.BHphThXsFLtg3bwZgYy82yXiOPJTbCIDZZ3I2HUAVjM",
 };
 
 window.addEventListener("DOMContentLoaded", () => {
